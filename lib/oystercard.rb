9bce904b0d0b970dd7f90330c44492cff11b7588
mackacavs <mackenziegdixon@yahoo.com>
2019-08-27 14:47:33 +0100

    Exercise 4

---
+++ lib/oystercard.rb
@@ -0,0 +1,7 @@
+class Oystercard
+  def initialize
+    @balance = 0
+  end
+  
+  attr_reader :balance
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,7 @@
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  it 'expects the oystercard to initialize with a balance of 0' do
+    expect(subject::balance).to eq 0
+  end
 end
