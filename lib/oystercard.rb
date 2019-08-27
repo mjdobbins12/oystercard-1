4415eb548d2f83b3ee7dee0d9030a34c7f39ce53
mackacavs <mackenziegdixon@yahoo.com>
2019-08-27 15:41:29 +0100

    Exercise 7 - Maximum Balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,13 @@
 class Oystercard
+
+  LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(money)
+    fail "Above limit value of #{LIMIT}" unless balance + money <= LIMIT
     @balance += money
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,9 +5,16 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-    describe '#top_up' do
+  describe '#top_up' do
     it 'tops up the balance with 5 ' do
       expect{ subject.top_up 5 }. to change{ subject.balance }.by 5
     end
+
+    it "expects to throw an exception if topup amount + balance exceeds limit" do
+      expect { subject.top_up(Oystercard::LIMIT + 1) }.to raise_error "Above limit value of #{Oystercard::LIMIT}"
     end
+  end
+
+
+
   end
