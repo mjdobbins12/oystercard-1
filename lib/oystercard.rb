1cc13ffaf9a5dee8c661d10f296190b6fabf2194
Gabriel Gonzalez <gabokappa@gmail.com>
2019-08-27 15:15:24 +0100

    Challenge 5 completed with top_up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,6 +2,10 @@ class Oystercard
   def initialize
     @balance = 0
   end
-  
+
+  def top_up(money)
+    @balance += money
+  end
+
   attr_reader :balance
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,12 @@ require 'oystercard'
 
 describe Oystercard do
   it 'expects the oystercard to initialize with a balance of 0' do
-    expect(subject::balance).to eq 0
+    expect(subject.balance).to eq 0
   end
-end
+
+    describe '#top_up' do
+    it 'tops up the balance with 5 ' do
+      expect{ subject.top_up 5 }. to change{ subject.balance }.by 5
+    end
+    end
+  end
